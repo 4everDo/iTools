8463d5e29c44b683370d1041d8a5b65b8767c81d
kuka <907376361@qq.com>
2019-07-24 14:28:45 +0800

    TBaseTableViewCell修改

--- iTools/Classes/Views/TBaseTableViewCell.h
+++ iTools/Classes/Views/TBaseTableViewCell.h
@@ -9,10 +9,16 @@
 #import <UIKit/UIKit.h>
 #import "TBaseModel.h"
 
+typedef struct HorizontalEdgeInsets {
+    CGFloat left,right;
+} HorizontalEdgeInsets;
+
 @interface TBaseTableViewCell : UITableViewCell
 
-@property (nonatomic,assign) BOOL  isHaveLine;
+@property (nonatomic,assign) BOOL  isSepatator;
 
-- (CGFloat)layoutHeightWithEntity:(TBaseModel *)entity;
+@property (nonatomic) HorizontalEdgeInsets separatorInsets;//分割线
+
++ (CGFloat)layoutHeightWithEntity:(TBaseModel *)entity;
 
 @end
--- iTools/Classes/Views/TBaseTableViewCell.m
+++ iTools/Classes/Views/TBaseTableViewCell.m
@@ -8,6 +8,12 @@
 
 #import "TBaseTableViewCell.h"
 
+@interface TBaseTableViewCell()
+{
+    UIView      *lineView;
+}
+@end
+
 @implementation TBaseTableViewCell
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
@@ -19,10 +25,26 @@
 }
 
 - (void)layoutBaseTableView{
-    
+    lineView = [[UIView alloc] initWithFrame:CGRectMake(0, self.height - 1, self.width, 1)];
+    lineView.backgroundColor = [UIColor colorWithHexString:@"#e1e1e1"];
+    lineView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleTopMargin;
+    [self.contentView addSubview:lineView];
 }
 
-- (CGFloat)layoutHeightWithEntity:(TBaseModel *)entity{
+- (void)setSeparatorInsets:(HorizontalEdgeInsets)separatorInsets {
+    _separatorInsets = separatorInsets;
+    lineView.left = separatorInsets.left;
+    lineView.right = separatorInsets.right;
+}
+
+- (void)setIsSepatator:(BOOL)isSepatator {
+    _isSepatator = isSepatator;
+    if (isSepatator) {
+        [self.contentView bringSubviewToFront:lineView];
+    }
+}
+
++ (CGFloat)layoutHeightWithEntity:(TBaseModel *)entity{
     return 50;
 }
 
